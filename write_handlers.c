cab1f339e0808f3f27ab36bdf8cc1959793979bd
Freanne <104914160+Freanne@users.noreply.github.com>
2023-04-28 21:32:03 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -93,7 +93,7 @@ int write_number(int is_negative, int ind, char buffer[],
  */
 int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
-	int length, char padd, char extra_c)
+	int len, char padd, char extra_c)
 {
 	int i, padd_start = 1;
 
@@ -101,40 +101,40 @@ int write_num(int ind, char buffer[],
 		return (0); /* printf(".0d", 0)  no char is printed */
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
-	if (prec > 0 && prec < length)
+	if (prec > 0 && prec < len)
 		padd = ' ';
-	while (prec > length)
-		buffer[--ind] = '0', length++;
+	while (prec > len)
+		buffer[--ind] = '0', len++;
 	if (extra_c != 0)
-		length++;
-	if (width > length)
+		len++;
+	if (width > len)
 	{
-		for (i = 1; i < width - length + 1; i++)
+		for (i = 1; i < width - len + 1; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
 		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+			return (write(1, &buffer[ind], len) + write(1, &buffer[1], i - 1));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], len));
 		}
 		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 		{
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
 			return (write(1, &buffer[padd_start], i - padd_start) +
-				write(1, &buffer[ind], length - (1 - padd_start)));
+				write(1, &buffer[ind], len - (1 - padd_start)));
 		}
 	}
 	if (extra_c)
 		buffer[--ind] = extra_c;
-	return (write(1, &buffer[ind], length));
+	return (write(1, &buffer[ind], len));
 }
 
 /**
