260608b2335629d702e26ab42ef1e943d496dbe8
Freanne <104914160+Freanne@users.noreply.github.com>
2023-04-28 21:26:20 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -10,19 +10,19 @@
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i;
+	int current_i;
 	int width = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[current_i]))
 		{
 			width *= 10;
-			width += format[curr_i] - '0';
+			width += format[current_i] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else if (format[current_i] == '*')
 		{
-			curr_i++;
+			current_i++;
 			width = va_arg(list, int);
 			break;
 		}
@@ -30,7 +30,7 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = current_i - 1;
 
 	return (width);
 }
