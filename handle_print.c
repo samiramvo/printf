638fb7ea5e1e35271a21b8dc46ab99a198d01dbc
Freanne <104914160+Freanne@users.noreply.github.com>
2023-04-28 21:23:45 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -14,7 +14,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_length = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -30,9 +30,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_length += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknow_length += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -42,8 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unknow_length += write(1, &fmt[*ind], 1);
-		return (unknow_length);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
