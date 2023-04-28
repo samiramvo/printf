25a5a3b07c34089ef6cfa8f895b85c0ee306f79d
Freanne <104914160+Freanne@users.noreply.github.com>
2023-04-28 21:12:38 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int i, print = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -37,11 +37,11 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			print = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
-			if (printed == -1)
+			if (print == -1)
 				return (-1);
-			printed_chars += printed;
+			printed_chars += print;
 		}
 	}
 
